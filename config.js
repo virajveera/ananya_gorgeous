ad692a202d56b13894851a0744e2f07368af0e70
virajveera <editvirajveera@gmail.com>
2025-02-13 22:31:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -86,7 +86,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dhquo5ttu/video/upload/v1739446347/MAKE_YOU_MINE_y4usjf.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
